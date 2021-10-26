6d9fc1816059c8f023262a119a189779d9584226
M-Rafiullah <mrafikhan2021@gmail.com>
2021-10-26 10:12:54 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,101 @@
+// alert("HELLO CLASS")
+
+// var name = "Sir Jaffar Awam";
+
+// var age = 24;
+
+// alert("My Name is" + name);
+
+
+//MATH OPT //
+//+ , - , / , * , % //
+
+
+//ADD//
+
+// var num1 = 450; //NUMBER
+// var num2 = 50; //NUMBER
+
+// var add = num1 + num2;
+
+// alert(add);
+
+
+//SUB//
+
+var num1 = 150; //NUMBER
+var num2 = 50; //NUMBER
+
+var sub = num1 - num2;
+         //150 - 50
+
+// alert(sub);
+
+//MUL//
+
+var num1 = 50;
+var num2 = 6;
+var mul = num1 * num2;
+
+// alert(mul);
+
+//DIV//
+
+var num1 = 142;
+var num2 = 4;
+var div =  num1 / num2;
+
+// alert(div)
+
+var num1 = 15;
+var num2 = 150;
+var per = num1 % num2;
+
+// alert(per)
+var firstname = prompt( "First Name")
+
+
+document.write("First Name :" + firstname +"<br>")
+
+var lastname = prompt( "last Name " )
+
+document.write("last Name :"+lastname +"<br>")
+
+var rollnum = prompt( "Roll Number")
+
+document.write("Roll Number :"+ rollnum +"<br>" )
+
+var schoolname = prompt( "School Name")
+
+document.write("School Name =" + schoolname +"<br>")
+
+var num = +prompt( "Islamiat")
+var a = num;
+document.write("Islamiat =" + a +"<br>" )
+
+var num1 = +prompt( "English")
+var b = num1;
+document.write("English =" + b +"<br>")
+
+var num2 = +prompt( "Urdu")
+var c = num2;
+document.write("Urdu =" + c +"<br>")
+
+var num3 = +prompt( "Javascript")
+var d = num3;
+document.write("Javascript =" + d +"<br>")
+
+var num4 = +prompt( "Html")
+var e = num4;
+document.write("Html  =" + e +"<br>")
+
+var obtained = a + b + c + d + e
+document.write("Obtain Marks ="+obtained +"<br>")
+
+var total = 500;
+ document.write("Total Marks =" + total +"<br>" )
+ 
+ var bmj =obtained;
+
+ var perc = bmj /total * 100
+ document.write("percentage =" + perc +"%") 
